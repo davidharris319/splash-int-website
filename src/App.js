f7fd08206a932038d84e6c681f0ce4b818968d7f
David Harris <davidharris319@gmail.com>
2021-03-13 17:06:06 -0500

    added background image to about section on home page, installed and added react-router-dom. added route to an about page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import "./styles/App.scss";
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from "./components/header";
 import Cases2 from "./components/cases2";
+import About from "./components/about";
+import AboutPage from "./pages/AboutPage/AboutPage";
 
 function App() {
   useEffect(() => {
@@ -15,6 +18,15 @@ function App() {
     <div className="App">
       <Header />
       <Cases2 />
+      <About/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/about' render={({history}) => <AboutPage
+          history={history}
+          />  
+        }/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/about.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/components/about.scss';
+
+const About = () => {
+  return (
+    <section className="about">
+      <div className="container">
+        <h2>About Splash International Marketing</h2>
+        <p>
+          Created back in 1985, with a vision to sell giftware, a mere five employees, a small office, undersized warehouse, and relocating several times over the years from building to building, Splash International Marketing has now landed as a top supplier situated in our very own 60,000 sq. ft. building in Markham, Ontario. With over 40 employees, 22 Sales Reps, and our very own Head Office/Showroom and attached warehouse off Highway #7 and Woodbine Avenue, Splash has proudly grown to be a well-established supplier in the giftware industry in Canada. 
+          </p>
+
+          <a>Learn More</a>
+      </div>
+    </section>
+  );
+};
+
+export default About;
