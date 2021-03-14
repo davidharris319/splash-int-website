34e7e032e7843118897979d6dd41aa36f362cc22
David Harris <davidharris319@gmail.com>
2021-03-14 19:59:35 -0400

    added contact/visit us section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,8 @@ import React from "react";
 import Cases2 from "../components/cases2";
 import About from "../components/About";
 import NewCustomer from "../components/NewCustomer";
+import Contact from "../components/Contact";
+
 
 
 function HomePage() {
@@ -11,6 +13,7 @@ function HomePage() {
       <Cases2 />
       <NewCustomer/>
       <About/>
+      <Contact/>
     </div>
   );
 }
