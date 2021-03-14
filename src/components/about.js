74faf893be77340f8741c107ae5d68973004b54d
David Harris <davidharris319@gmail.com>
2021-03-13 22:01:58 -0500

    updated styling of about

--- src/components/about.js
+++ src/components/about.js
@@ -6,13 +6,13 @@ const About = () => {
   return (
     <section className="about-container">
       <div className="about-background">
-        <div className="text-box">
-          <h2>About Splash International Marketing</h2>
-          <p>
-            Created back in 1985, with a vision to sell giftware, a mere five employees, a small office and an undersized warehouse, Splash International Marketing is now considered one of the top suppliers in Canada.
-            </p>
-            <Link to='/about'>Learn More</Link>
-        </div>
+          <div className="text-box">
+            <h2>About Splash International Marketing</h2>
+            <p>
+              Created back in 1985, with a vision to sell giftware, a mere five employees, a small office and an undersized warehouse, Splash International Marketing is now considered one of the top suppliers in Canada.
+              </p>
+              <Link to='/about'>Learn More</Link>
+          </div>
       </div>
 
     </section>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,7 +8,9 @@ function HomePage() {
     <div className="HomePage">
       <Cases2 />
       <About/>
-
+      <div>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/BCWJyZQED9E" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
     </div>
   );
 }
